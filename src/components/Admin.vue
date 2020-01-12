9b5d875c132bce1f1c787356720e90c5c2f4bd7c
= <=>
2020-01-12 10:41:42 -0800

    Add User Button

--- src/components/Admin.vue
+++ src/components/Admin.vue
@@ -37,6 +37,7 @@
         </div>
         </section>
         <section class="section">
+            <button class="button2">Add User</button>
     <div id ="container">
 
         <div class="search-container">
@@ -160,5 +161,40 @@
         width: 63em;
         margin: 1em;
 
+    }
+    .button2 {
+        position: relative;
+        background-color: #4CAF50;
+        border: none;
+        font-size: 28px;
+        color: #FFFFFF;
+        padding: 20px;
+        width: 200px;
+        text-align: center;
+        -webkit-transition-duration: 0.4s; /* Safari */
+        transition-duration: 0.4s;
+        text-decoration: none;
+        overflow: hidden;
+        cursor: pointer;
+    }
+
+    .button2:after {
+        content: "";
+        background: #90EE90;
+        display: block;
+        position: absolute;
+        padding-top: 300%;
+        padding-left: 350%;
+        margin-left: -20px!important;
+        margin-top: -120%;
+        opacity: 0;
+        transition: all 0.8s
+    }
+
+    .button2:active:after {
+        padding: 0;
+        margin: 0;
+        opacity: 1;
+        transition: 0s
     }
 </style>
