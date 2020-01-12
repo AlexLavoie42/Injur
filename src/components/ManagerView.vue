c9dfd78f69d0ca60b36a5ef3a5d32e74b4975de9
= <=>
2020-01-11 19:43:08 -0800

    Manager View Buttons

--- src/components/ManagerView.vue
+++ src/components/ManagerView.vue
@@ -1,5 +1,16 @@
 <template>
-    
+    <div class="container">
+        <router-link to="ManagerForm"><button class="button" style="vertical-align:middle">
+            <span>Submit an Incident </span></button></router-link>
+
+        <button class="button" style="vertical-align:middle"><span>Account Administration</span></button>
+
+
+    <button class="button" style="vertical-align:middle"><span>View Incidents</span></button>
+
+    <button class="button" style="vertical-align:middle"><span>View Tips</span></button>
+    </div>
+
 </template>
 
 <script>
@@ -9,5 +20,48 @@
 </script>
 
 <style scoped>
+    .button {
+        display: inline-block;
+        border-radius: 4px;
+        background-color: #61afe0;
+        border: none;
+        color: #FFFFFF;
+        text-align: center;
+        font-size: 28px;
+        padding: 20px;
+        width: 80%;
+        transition: all 0.5s;
+        cursor: pointer;
+        margin: auto;
+        margin-top: 1em !important;
+        display:block;
+        font-family: 'Open Sans', sans-serif;
+
+    }
+
+    .button span {
+        cursor: pointer;
+        display: inline-block;
+        position: relative;
+        transition: 0.5s;
+    }
+
+    .button span:after {
+        content: '\00bb';
+        position: absolute;
+        opacity: 0;
+        top: 0;
+        right: -20px;
+        transition: 0.5s;
+    }
+
+    .button:hover span {
+        padding-right: 25px;
+    }
+
+    .button:hover span:after {
+        opacity: 1;
+        right: 0;
+    }
 
 </style>
